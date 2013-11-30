0862bf3475e83e30579e0b6df32af8ab85d406ee
Jeremiah Mahler <jmmahler@gmail.com>
2013-11-29 22:49:37 -0800

    engine model with no control working
    With the torque input is set to zero a transfer function
    representing the engine model can be constructed.
    This should be suitable for building control systems
    using pole placements or other methods.

---
+++ octave/engine_model.m
@@ -0,0 +1,17 @@
+% 
+% engine_model.m
+%
+% Linear engine model with zero torque.
+%
+
+T = 1;  % time step
+
+K = tf([1.699], [1], T);
+D1= tf([8.5683], [1 -0.9025 0], T);
+D2= tf([2.98], [1 -0.9354], T);
+G = D1*D2;
+D = tf([0.00093], [1], T);
+
+H = K*G/(1 + D*G);
+
+eng_sys = minreal(H);  % cancel common roots
